01a7a8ea5af5943aa96758348f6d5bf2623e76ec
Alexis Montes <montes.alexis25@gmail.com>
2021-06-13 19:17:25 -0400

    finished intitial version

--- server.js
+++ server.js
@@ -3,12 +3,34 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const mongoose = require('mongoose')
+
+const userName = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+const URL = "mongodb+srv://"+userName+":"+password+"@cluster0.vrici.mongodb.net/Mechanical_switches?retryWrites=true&w=majority";
+
+// connect to database
+mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("There was some sort of error in connecting to the database");
+    
+  })
+
+const urls = require('./models/urls');
+const { json } = require('body-parser');
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use(express.json())
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +40,57 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+app.post('/api/shorturl', function(req, res) {
+  const urlRegex = /((http(s)?:\/\/.)(www\.)?){1}[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g 
+  let new_url = req.body.original_url;
+  let shortcut_url = getRandomInt(Number.MAX_SAFE_INTEGER).toString();
+
+  // checks if url string is actualy a valid url
+  if(new_url.match(urlRegex) !== null && new_url.match(urlRegex).length == 1){
+    console.log("url reads as an actual url")
+
+    // check to see if existing shortcut url is already in database and corrects for it
+    urls.find({})
+    .then((results) => {
+      for(url in results) {
+        if(url.short_url === shortcut_url){
+          console.log("not valid short url")
+          shortcut_url = getRandomInt(Number.MAX_SAFE_INTEGER).toString();
+          
+        }
+      }   
+    }) 
+
+    // adds the url to the database
+    urls.create({original_url: new_url, short_url: shortcut_url}, (err, urls) => {
+      if(err){
+        res.send({error: 'database error'})
+        return;
+      }
+
+      res.send({original_url: urls.original_url, short_url: urls.short_url})
+      return;
+    })
+
+  } else {
+    res.send({error: 'invalid url'})
+  }
+  
+
+})
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  
+  urls.findOne({short_url: req.params.short_url}, (err,result) => {
+    res.redirect(result.original_url)
+  })
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
