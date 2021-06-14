532308b7cb372458908b13e1cc671acefdcc60c4
Alexis Montes <montes.alexis25@gmail.com>
2021-06-13 23:08:53 -0400

    added secondary post route

--- server.js
+++ server.js
@@ -85,6 +85,46 @@ app.post('/api/shorturl', function(req, res) {
 
 })
 
+app.post('/api/shorturl/new', function(req, res) {
+  const urlRegex = /((http(s)?:\/\/.)(www\.)?){1}[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g 
+  let new_url = req.body.url;
+  console.log(req.body)
+  let shortcut_url = getRandomInt(Number.MAX_SAFE_INTEGER).toString();
+
+  // checks if url string is actualy a valid url
+  if(new_url.match(urlRegex) !== null && new_url.match(urlRegex).length == 1){
+    console.log("url reads as an actual url")
+
+    // check to see if existing shortcut url is already in database and corrects for it
+    urls.find({})
+    .then((results) => {
+      for(url in results) {
+        if(url.short_url === shortcut_url){
+          console.log("not valid short url")
+          shortcut_url = getRandomInt(Number.MAX_SAFE_INTEGER).toString();
+          
+        }
+      }   
+    }) 
+
+    // adds the url to the database
+    urls.create({original_url: new_url, short_url: shortcut_url}, (err, urls) => {
+      if(err){
+        res.send({error: 'database error'})
+        return;
+      }
+
+      res.send({original_url: urls.original_url, short_url: parseInt(urls.short_url)});
+      return;
+    })
+
+  } else {
+    res.send({ error: 'invalid url' })
+  }
+  
+
+})
+
 app.get('/api/shorturl/:short_url', function(req, res) {
   
   urls.findOne({short_url: req.params.short_url}, (err,result) => {
